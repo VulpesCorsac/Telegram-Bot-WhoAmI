c4c998d03781ccd7972e5934cd4c4d7bdb05d6ea
George Aloyan <shaxirrim@gmail.com>
2024-03-31 14:55:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot.set_my_name('WhoAmI')
 bot.set_my_short_description('This bot will send you back your telegram information')
 bot.set_my_description('This bot will send you back your telegram information: id, name, surname, language, premium...')
 bot.set_my_commands([
-    telebot.types.BotCommand('/start', 'Greet user with his id or hardcoded name'),
+    telebot.types.BotCommand('/start', 'Greet user with his tg name'),
     telebot.types.BotCommand('/who_am_i', 'Print user information'),
 ])
 
